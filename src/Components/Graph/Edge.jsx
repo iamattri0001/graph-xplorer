24d4bd6e26ca538ba01b23405a17ec00e38b50ca
iamattri0001 <iamattri0001@gmail.com>
2023-09-24 00:26:45 +0530

    diable edge transformation

--- src/Components/Graph/Edge.jsx
+++ src/Components/Graph/Edge.jsx
@@ -16,23 +16,19 @@ const Edge = ({
 
   return (
     <g className="edge stroke-edge" datafrom={fromName} datato={toName}>
-      <g
-        transform={`translate(${from.x}, ${from.y})`}
-        className="transition-transform duration-500 ease"
-      >
-        <line
-          x1={0}
-          y1={0}
-          x2={to.x - from.x} // Calculate the new x2 based on the translation
-          y2={to.y - from.y} // Calculate the new y2 based on the translation
-          strokeWidth={1.3}
-        />
-      </g>
+      <line
+        x1={from.x}
+        y1={from.y}
+        x2={to.x} // Calculate the new x2 based on the translation
+        y2={to.y} // Calculate the new y2 based on the translation
+        strokeWidth={1.3}
+      />
 
       {isWeighted && (
         <text
           className="stroke-none fill-wedgewood-50 text-md transition-all duration-700"
-          transform={`translate(${textX}, ${textY})`}
+          x={textX}
+          y={textY}
         >
           {Math.ceil(
             (Math.sqrt(
